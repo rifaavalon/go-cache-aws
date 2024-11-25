205d3db53c062b2f1420088e9b2f9ee430237820
Chris <chris.hickey@vidence.com>
2024-11-25 12:06:30 -0700

    redoing routes

--- handlers.go
+++ handlers.go
@@ -13,16 +13,19 @@ import (
 	mux "github.com/julienschmidt/httprouter"
 )
 
+// LogHttp logs the details of the HTTP request
 func LogHttp(r *http.Request) {
 	start := time.Now()
 	log.Printf("%s\t%s\t%q\t%s", r.Method, r.RequestURI, r.Header, time.Since(start))
 }
 
+// Index handles the root endpoint
 func Index(w http.ResponseWriter, r *http.Request, _ mux.Params) {
 	LogHttp(r)
 	fmt.Fprintf(w, "<h1 style=\"font-family: Helvetica;\">Welcome to the Xero AWS Cache</h1>")
 }
 
+// InstanceIndex handles the /instances endpoint
 func InstanceIndex(w http.ResponseWriter, r *http.Request, _ mux.Params) {
 	LogHttp(r)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -36,9 +39,10 @@ func InstanceIndex(w http.ResponseWriter, r *http.Request, _ mux.Params) {
 	}
 }
 
+// InstanceShow handles the /instances/:id endpoint
 func InstanceShow(w http.ResponseWriter, r *http.Request, ps mux.Params) {
 	LogHttp(r)
-	id, err := strconv.Atoi(ps.ByName("instanceIds"))
+	id, err := strconv.Atoi(ps.ByName("instanceId"))
 	if err != nil {
 		http.Error(w, "Invalid instance ID", http.StatusBadRequest)
 		return
@@ -52,6 +56,7 @@ func InstanceShow(w http.ResponseWriter, r *http.Request, ps mux.Params) {
 	}
 }
 
+// InstanceCreate handles the creation of a new instance
 func InstanceCreate(w http.ResponseWriter, r *http.Request, _ mux.Params) {
 	var instance Instance
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
